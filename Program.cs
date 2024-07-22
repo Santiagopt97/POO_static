97fb157c68f713078061e8266c71b0cc897189d5
Santiagopt97 <santiagopineda99@gmail.com>
2024-07-22 06:49:05 -0500

    Feat: se agrego el ejemplo de uso de propiedades y metodos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using POO_static.Models;
+
+//Uso de propiedades y metodos
+Console.Clear();
+var ejemplo1 = new Ejemplo();
+Console.WriteLine(ejemplo1.Nombre);
+Console.WriteLine(ejemplo1.Apellido);
+
+ejemplo1.Saludar();
