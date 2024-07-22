732a635a6473b328e485bc74368ad9b3a177949e
Santiagopt97 <santiagopineda99@gmail.com>
2024-07-22 07:26:30 -0500

    feat: Uso de static

--- Models/EjemploEstatico.cs
+++ Models/EjemploEstatico.cs
@@ -5,18 +5,18 @@ using System.Threading.Tasks;
 
 namespace POO_static.Models;
 
-    public class EjemploEstatico
+    public static class EjemploEstatico //Si la clase es estatica, todas sus propiedades y metodos si o si deben ser estaticos, si se deja la clase normal permite el uso hibrido de estatico y no estatico
     {
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
+        public static string Nombre { get; set; }
+        public static string Apellido { get; set; }
         
-        public EjemploEstatico()
+        static EjemploEstatico()
         {
             Nombre = "Santiago";
             Apellido = "Pineda";
         }
 
-        public void Saludar(){
+        public static void Saludar(){
             Console.WriteLine($"Hola, mi nombre es {Nombre} {Apellido}");
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿using POO_static.Models;
+//El static permite acceder a las propiedades y metodos estaticos de una clase sin tener que instanciar la clase.
 
 //Uso de propiedades y metodos
-Console.Clear();
-var ejemplo1 = new Ejemplo();
-Console.WriteLine(ejemplo1.Nombre);
-Console.WriteLine(ejemplo1.Apellido);
+// Console.Clear();
+// var ejemplo1 = new Ejemplo();
+// Console.WriteLine(ejemplo1.Nombre);
+// Console.WriteLine(ejemplo1.Apellido);
 
-ejemplo1.Saludar();
+// ejemplo1.Saludar();
+
+// //Uso de propiedades y metodos con static
+// EjemploEstatico.Nombre = "david";
+// EjemploEstatico.Apellido = "Pineda";
+
+// Console.WriteLine($"Nombre: {EjemploEstatico.Nombre}");
+// Console.WriteLine($"Nombre: {EjemploEstatico.Apellido}");
+// EjemploEstatico.Saludar();
+
+//Uso de constructor estatico
+EjemploEstatico.Saludar();
